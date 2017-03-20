d006150a2854fe8eaa0b4eac612d3c74731ef4fc
Saulius Lukauskas <luksaulius@gmail.com>
2017-03-20 12:56:42 +0000

    initial structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lopc',
+    version='0.1',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    url='https://github.com/lukauskas/python-lopc',
+    license='MIT',
+    author='Saulius Lukauskas',
+    author_email='saulius.lukauskas13@imperial.ac.uk',
+    description=''
+)
